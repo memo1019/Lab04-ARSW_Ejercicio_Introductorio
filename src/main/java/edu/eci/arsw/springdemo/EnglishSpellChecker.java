6e26f8869ce49d3d9cd4e950a7cd0697e50da724
Guillermo Castro <guillermo.castro@mail.escuelaing.edu.co>
2021-02-10 08:42:24 -0500

    ejercicio finalizado

--- src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/EnglishSpellChecker.java
@@ -1,10 +1,15 @@
 package edu.eci.arsw.springdemo;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Qualifier;
 
+
+
+@Service("englishSpellChecker")
 public class EnglishSpellChecker implements SpellChecker {
 
 	@Override
 	public String checkSpell(String text) {		
-		return "Checked with english checker:"+text;
+		return "Checked with english checker: "+text;
 	}
 
         
--- src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
+++ src/main/java/edu/eci/arsw/springdemo/SpanishSpellChecker.java
@@ -2,12 +2,14 @@ package edu.eci.arsw.springdemo;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.springframework.stereotype.Service;
 
+@Service("spanishSpellChecker")
 public class SpanishSpellChecker implements SpellChecker {
 
 	@Override
 	public String checkSpell(String text) {
-		return "revisando ("+text+") con el verificador de sintaxis del espanol";
+		return "revisando ("+text+") con el verificador de sintaxis del espanol. ";
                 
                 
 	}
